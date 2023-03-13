b285b5985a1c1b5d8d811730e50d6d7cd9b296db
Mark Wlodawski <imakethesites@gmail.com>
2023-03-13 14:16:06 -0400

    Add blog population code to blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,35 @@
 import Page from '@/components/Page';
+import Link from 'next/link';
+import groq from 'groq';
+import client from '@/client';
 
-function BlogPage() {
-  return <Page title="Blog"></Page>;
+function BlogPage({ posts }) {
+  return (
+    <Page title="Blog">
+      {posts.length > 0 &&
+        posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href={`/post/${encodeURIComponent(slug.current)}`}>
+                  {title}
+                </Link>{' '}
+                ({new Date(publishedAt).toDateString()})
+              </li>
+            )
+        )}
+    </Page>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+  *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+  `);
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 export default BlogPage;

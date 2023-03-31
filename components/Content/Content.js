143aad83f915540dff8c76118fc47ae2d7f9414d
Mark Wlodawski <imakethesites@gmail.com>
2023-03-30 21:52:55 -0400

    Add slug page styling

--- components/Content/Content.js
+++ components/Content/Content.js
@@ -1,9 +1,21 @@
 import cl from 'classnames';
-
+import SanityBlockContent from '@sanity/block-content-to-react';
 import styles from './Content.module.scss';
+import { clientConfig } from '@/lib/client';
 
-const Content = ({ children, className }) => {
-  return <div className={cl(className, styles.content)}>{children}</div>;
+const Content = ({ body, className }) => {
+  console.log('body ===>', body);
+
+  return (
+    <div className={cl(className, styles.content)}>
+      <SanityBlockContent
+        blocks={body}
+        imageOptions={{ w: 1000, h: 750, fit: 'max' }}
+        projectId={clientConfig.projectId}
+        dataset={clientConfig.dataset}
+      />
+    </div>
+  );
 };
 
 export default Content;
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,11 +2,18 @@ import SanityBlockContent from '@sanity/block-content-to-react';
 import { useNextSanityImage } from 'next-sanity-image';
 import Link from 'next/link';
 import Image from 'next/image';
-import { sanityClient } from '@/sanityClient';
+import { client } from '@/lib/client';
+import Article from '@/components/Article/Article';
+import Title from '@/components/Title/Title';
+import Content from '@/components/Content/Content';
+import styles from './PostStyles.module.scss';
+import { format, parseISO } from 'date-fns';
+// import styles
 
-const Post = (props) => {
-  const { title, body, image } = props;
-  const imageProps = useNextSanityImage(sanityClient, image);
+const Post = ({ post }) => {
+  // const date = format(new Date(post.publishedDate), 'MM/dd/yyyy');
+
+  // const imageProps = useNextSanityImage(sanityClient, image);
 
   return (
     <div className="container py-5 px-3">
@@ -30,49 +37,84 @@ const Post = (props) => {
           </svg>
 
           <li className="inline-flex items-center" aria-current="page">
-            {title}
+            {post.title}
           </li>
         </ol>
       </nav>
-      <article>
+      <Article className={styles.post}>
+        <Title className={styles.postTitle}>{post.title}</Title>
+        {/* <p className={styles.postDate}>{date}</p> */}
+        <Content body={post.body} />
+      </Article>
+      {/* <article>
         <h1 className="text-lg">{title}</h1>
         {imageProps && <Image {...imageProps} layout="intrinsic" alt="" />}
         <SanityBlockContent blocks={body} />
-      </article>
+      </article> */}
     </div>
   );
 };
 
-export const getServerSideProps = async (context) => {
-  const pageSlug = context.query.slug;
+export async function getStaticPaths() {
+  const query = `*[ _type == "post"] {
+    slug {
+      current
+    }
+  }`;
 
-  if (!pageSlug) {
-    return {
-      notFound: true,
-    };
-  }
+  const posts = await client.fetch(query);
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
 
-  const query = encodeURIComponent(
-    `*[ _type == "post" && slug.current == "${pageSlug}"]`
-  );
-  const url = `${process.env.SANITY_URL}query=${query}`;
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+}
 
-  const data = await fetch(url).then((res) => res.json());
-  const post = data.result[0];
+export async function getStaticProps({ params: { slug } }) {
+  const query = `*[ _type == "post" && slug.current == "${slug}"][0]`;
 
-  if (!post) {
-    return {
-      notFound: true,
-    };
-  } else {
-    return {
-      props: {
-        title: post.title,
-        body: post.body,
-        image: post.mainImage,
-      },
-    };
-  }
-};
+  const post = await client.fetch(query);
+
+  return {
+    props: {
+      post,
+    },
+  };
+
+  // const pageSlug = context.query.slug;
+
+  // if (!pageSlug) {
+  //   return {
+  //     notFound: true,
+  //   };
+  // }
+
+  // const query = encodeURIComponent(
+  //   `*[ _type == "post" && slug.current == "${pageSlug}"]`
+  // );
+  // const url = `${process.env.SANITY_URL}query=${query}`;
+
+  // const data = await fetch(url).then((res) => res.json());
+  // const post = data.result[0];
+
+  // if (!post) {
+  //   return {
+  //     notFound: true,
+  //   };
+  // } else {
+  //   return {
+  //     props: {
+  //       title: post.title,
+  //       body: post.body,
+  //       image: post.mainImage,
+  //     },
+  //   };
+  // }
+}
 
 export default Post;

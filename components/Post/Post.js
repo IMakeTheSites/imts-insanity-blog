8a1c683e5479f5a9ce0b618f239d81be1bd705de
Mark Wlodawski <imakethesites@gmail.com>
2023-04-01 22:36:30 -0400

    Add imageurl requirements and begin cleaning out commented code

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,12 +1,34 @@
 import cl from 'classnames';
 import Link from 'next/link';
 import Image from 'next/image';
-
+import imageUrlBuilder from '@sanity/image-url';
 import styles from './Post.module.scss';
 import Title from '../Title/Title';
-import { urlFor } from '../../lib/client';
+import { PortableText } from '@portabletext/react';
+import { client } from '@/lib/client';
 
-const Post = ({ className, image, title, description, slug }) => {
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source);
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value }) => {
+      if (!value?.asset?._ref) {
+        return null;
+      }
+      return (
+        <img
+          alt={value.alt || ''}
+          loading="lazy"
+          src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+        />
+      );
+    },
+  },
+};
+
+const Post = ({ className, body = [], title, description, slug }) => {
   console.log(slug);
   return (
     <Link
@@ -19,12 +41,7 @@ const Post = ({ className, image, title, description, slug }) => {
         </Title>
         <div className={styles.postContent}>
           <div className={styles.postImage}>
-            <Image
-              src={urlFor(image).url()}
-              alt={image.caption}
-              width="100"
-              height="100"
-            />
+            <PortableText value={body} components={ptComponents} />
           </div>
           <p className={styles.postDescription}>{description}</p>
         </div>
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,21 +1,49 @@
-import BlockContent from '@sanity/block-content-to-react';
-import { useNextSanityImage } from 'next-sanity-image';
+import { PortableText } from '@portabletext/react';
 import Link from 'next/link';
-import Image from 'next/image';
+import imageUrlBuilder from '@sanity/image-url';
 import { client } from '@/lib/client';
 import Article from '@/components/Article/Article';
 import Title from '@/components/Title/Title';
-import Content from '@/components/Content/Content';
 import styles from './PostStyles.module.scss';
-import { format } from 'date-fns';
+import groq from 'groq';
+import cl from 'classnames';
+import React from 'react';
+import { FaGreaterThan } from 'react-icons/fa';
 
-// import styles
+function urlFor(imageUrl) {
+  return imageUrlBuilder(client).image(imageUrl);
+}
 
-const Post = ({ post }) => {
-  // const date = format(new Date(post.publishedDate), 'MM/dd/yyyy');
+const ptComponents = {
+  types: {
+    image: ({ imageUrl }) => {
+      if (!imageUrl?.asset?._ref) {
+        return null;
+      }
+      return (
+        <img
+          alt={imageUrl.alt || ''}
+          loading="lazy"
+          src={urlFor(imageUrl)
+            .width(320)
+            .height(240)
+            .fit('max')
+            .auto('format')}
+        />
+      );
+    },
+  },
+};
 
-  // const imageProps = useNextSanityImage(sanityClient, image);
+const Post = ({ className, post }) => {
+  const {
+    title = 'Missing title',
+    name = 'Missing name',
+    imageUrl,
+    body = [],
+  } = post;
 
+  console.log('post ===>', post);
   return (
     <div className="container py-5 px-3">
       <nav className="flex" aria-label="breadcrumb">
@@ -23,63 +51,52 @@ const Post = ({ post }) => {
           <li className="inline-flex items-center">
             <Link href="/blog">Blog</Link>
           </li>
-          <svg
-            aria-hidden="true"
-            class="w-6 h-6 text-gray-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-
+          {React.createElement(FaGreaterThan, {
+            color: 'gray',
+            size: 20,
+          })}
           <li className="inline-flex items-center" aria-current="page">
             {post.title}
           </li>
         </ol>
       </nav>
       <Article className={styles.post}>
-        <Title className={styles.postTitle}>{post.title}</Title>
-        {/* <p className={styles.postDate}>{date}</p> */}
-        <Content body={post.body} />
+        <Title className={styles.postTitle}>{title}</Title>
+        <div className={cl(className, styles.content)}>
+          {imageUrl && (
+            <div className={cl(className, styles.content)}>
+              <img src={urlFor(imageUrl).url()} alt={`${title}`} />
+            </div>
+          )}
+          <PortableText value={body} components={ptComponents} />
+        </div>
       </Article>
-      {/* <article>
-        <h1 className="text-lg">{title}</h1>
-        {imageProps && <Image {...imageProps} layout="intrinsic" alt="" />}
-        <SanityBlockContent blocks={body} />
-      </article> */}
     </div>
   );
 };
 
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  "imageUrl": image.asset->url,
+  body
+}`;
+
 export async function getStaticPaths() {
-  const query = `*[_type == "post"] {
-    slug {
-      current
-    }
-  }`;
-
-  const posts = await client.fetch(query);
-  const paths = posts.map((post) => ({
-    params: {
-      slug: post.slug.current,
-    },
-  }));
-
+  const paths = await client.fetch(
+    groq`*[ _type == "post" && defined(slug.current)][].slug.current`
+  );
   return {
-    paths,
-    fallback: 'blocking',
+    paths: paths.map((slug) => ({ params: { slug } })),
+    fallback: true,
   };
 }
 
-export async function getStaticProps({ params: { slug } }) {
-  const query = `*[_type == "post" && slug.current == "${slug}"][0]`;
-
-  const post = await client.fetch(query);
+export async function getStaticProps(context) {
+  const { slug = '' } = context.params;
+  const post = await client.fetch(query, { slug });
 
   return {
     props: {
@@ -87,35 +104,5 @@ export async function getStaticProps({ params: { slug } }) {
     },
   };
 }
-
-// const pageSlug = context.query.slug;
-
-// if (!pageSlug) {
-//   return {
-//     notFound: true,
-//   };
-// }
-
-// const query = encodeURIComponent(
-//   `*[ _type == "post" && slug.current == "${pageSlug}"]`
-// );
-// const url = `${process.env.SANITY_URL}query=${query}`;
-
-// const data = await fetch(url).then((res) => res.json());
-// const post = data.result[0];
-
-// if (!post) {
-//   return {
-//     notFound: true,
-//   };
-// } else {
-//   return {
-//     props: {
-//       title: post.title,
-//       body: post.body,
-//       image: post.mainImage,
-//     },
-//   };
-// }
 
 export default Post;
